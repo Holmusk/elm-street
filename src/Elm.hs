a8a749581052a2e8a4a9bdcbbbeaf2f96ff01f95
Dmitrii Kovanikov <kovanikov@gmail.com>
2019-01-30 10:52:56 +0800

    [#2] Implement AST (#11)

---
+++ src/Elm.hs
@@ -0,0 +1,8 @@
+{- | Library for generic conversion of data types from Haskell to Elm.
+-}
+
+module Elm
+       ( module Elm.Ast
+       ) where
+
+import Elm.Ast
--- src/ElmStreet.hs
+++
@@ -1,6 +0,0 @@
-module ElmStreet
-       ( someFunc
-       ) where
-
-someFunc :: IO ()
-someFunc = putStrLn ("someFunc" :: String)
